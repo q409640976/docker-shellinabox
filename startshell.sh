aadcc677a032b6fb0b69e7ff2d4ffc8babaabb3d
q409640976 <q409640976@gmail.com>
2020-07-08 21:10:17 +0800

    Update startshell.sh

--- startshell.sh
+++ startshell.sh
@@ -10,7 +10,7 @@ else
   echo $USERPWD | passwd root --stdin
 fi
 # Este comando sempre entra como user (nao tem login)
-shellinaboxd --no-beep -t --user-css Normal:+/usr/share/shellinabox/black-on-white.css,Reverse:-/usr/share/shellinabox/white-on-black.css --service "/:user:users:/home/user:/bin/bash"
+shellinaboxd --no-beep -t --user-css Normal:+/usr/share/shellinabox/black-on-white.css,Reverse:-/usr/share/shellinabox/white-on-black.css --service "/:root:root:/www:/bin/bash"
 # Este tem login
 #shellinaboxd --no-beep -t -s "/:LOGIN"
 
